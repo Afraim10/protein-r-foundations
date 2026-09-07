2c0816189c78ecc50a6a6c3b763e273b3a712c33
Afraim Mario <afraim.mario@outlook.com>
2026-09-07 21:04:57 +0300

    Fixing Residue Position Indexing.

--- checkpoints/checkpoint04_residue_indexing.R
+++ checkpoints/checkpoint04_residue_indexing.R
@@ -11,11 +11,12 @@ tyrosines_positions <- which(prot_seq_splitted == "Y")
 
 phosphorylatable_residues <- c("S", "T", "Y")
   
-phosphorylatable_residues %in% c(prot_seq_splitted[serine_positions],prot_seq_splitted[threonine_positions], prot_seq_splitted[tyrosines_positions])
+logical_check <- c(prot_seq_splitted[serine_positions],prot_seq_splitted[threonine_positions], prot_seq_splitted[tyrosines_positions]) %in% phosphorylatable_residues
+print(logical_check)
 
-total_phosphorylatable <- sum(c(length(serine_positions), length(threonine_positions), length(tyrosines_pos)))
+total_phosphorylatable <- sum(c(length(serine_positions), length(threonine_positions), length(tyrosines_positions)))
 
-cat("Total phosphorylatable residues:", total_phosphorylatable)
-cat("Serine found at:", serine_positions)
-cat("Threonine found at:", threonine_positions)
-cat("Tyrosine found at:", tyrosines_positions)
+cat("Total phosphorylatable residues:", total_phosphorylatable,"\n")
+cat("Serine found at:", serine_positions,"\n")
+cat("Threonine found at:", threonine_positions,"\n")
+cat("Tyrosine found at:", tyrosines_positions,"\n")
